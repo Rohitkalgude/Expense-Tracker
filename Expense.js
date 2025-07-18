dae80438475a10dbc0e016c7e7ec6cd86c848512
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-07-18 19:25:36 +0530

    update

---
+++ Expense.js
@@ -0,0 +1,7 @@
+const BalanceEl = document.getElementById("balance");
+const Income = document.getElementById("income-amount");
+const Expence = document.getElementById("expence-amount");
+const Tranctionlist = document.getElementById("tranction-list");
+const Tranctionform = document.getElementById("tranction-form");
+const Description = document.getElementById("description");
+const Amount = document.getElementById("amount");
